51e4172f66e479a625c152a53a361facd2c4ecef
Parker Merritt <parkermerritt@Parkers-MacBook-Pro.local>
2018-08-25 14:05:27 -0400

    Add video background to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import YouTube from 'react-youtube'
+import '../assets/scss/main.scss'
 
 class Header extends React.Component {
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    // event.target.mute();
+  }
+
+  _onEnd(event) {
+    event.target.playVideo()
+  }
   render() {
+    const videoOptions = {
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+        controls: 0,
+        rel: 0,
+        showinfo: 0,
+      },
+    }
+
     return (
       <section id="header">
+        <div className="video-background">
+          <div className="video-foreground">
+            <YouTube
+              videoId="-kLOn9unUAw"
+              opts={videoOptions}
+              className="video-iframe"
+              onReady={this._onReady}
+              onEnd={this._onEnd}
+            />
+          </div>
+        </div>
         <div className="inner">
           <span className="icon major fa-home" />
           <h1>
